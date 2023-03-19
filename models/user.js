5dfeb33252e603305f3a8ce06d17c5d80215ec3a
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-03-19 14:04:20 +0200

    Import bcryptjs package and edit User model
    	- Import bcrypt package for password hashing
    	- Create a pre 'save' hook to rehash the password if it modified
    	- Create a function to check user credentials while login
    	  (username, password)

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { strLengthValidator } = require('./validators');
 
 const userSchema = new mongoose.Schema({
@@ -11,6 +12,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    minLength: [6, 'Password must be at least 6 characters'],
   },
   firstName: {
     type: String,
@@ -22,6 +24,20 @@ const userSchema = new mongoose.Schema({
     type: Number,
     min: [13, 'Minimum age is 13 years old'],
   },
+});
+
+userSchema.statics.findByCredentials = async function (username, password) {
+  const user = await this.find({ username });
+  if (!user) throw new Error('Username is not registered');
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error('Password is not correct');
+  return user;
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 const User = new mongoose.model('User', userSchema);
